87d7e2e4adf2633eb44bd277befc67053cc00bb3
kooseolwon <seolwon6289@naver.com>
2018-09-14 06:38:44 +0900

    시간 포멧 바꾸기...

--- gradProj/routes/comment/comment.js
+++ gradProj/routes/comment/comment.js
@@ -12,15 +12,15 @@ router.post('/write', async function(req,res){
     //var comment_photo = req.body.comment_photo;
     var board_index = req.body.board_index;
     var user_index = req.body.user_index;
-    var comment_time = moment().format('YYYY-MM-DD HH:mm:ss')
+    //var comment_time = moment().format('YYYY-MM-DD HH:mm:ss')
 
-    if(!comment_content || !comment_time ||!board_index ||!user_index){
+    if(!comment_content ||!board_index ||!user_index){
         res.status(400).send({
             message:"fail writing comment from client"
         });
     }else{
-        var commentQuery = 'INSERT INTO comment_table (board_index,comment_content,user_index,comment_time) values (?,?,?,?)';
-        var commentResult = await pool.queryParam_Arr(commentQuery, [board_index,comment_content,user_index,comment_time]);
+        var commentQuery = 'INSERT INTO comment_table (board_index,comment_content,user_index) values (?,?,?)';
+        var commentResult = await pool.queryParam_Arr(commentQuery, [board_index,comment_content,user_index]);
         console.log(commentResult);
         if(!commentResult){
             res.status(500).send({
@@ -47,7 +47,9 @@ router.get('/show/:board_index',async function(req,res){
             message:"fail showing comment from client"
         });
     }else{
-        var commentQuery = "SELECT comment_index,comment_content,comment_time,user_name FROM comment_table JOIN user_table ON comment_table.user_index = user_table.user_index WHERE board_index=?";
+        var commentQuery = `SELECT comment_index,comment_content,
+        date_format(comment_time ,"%Y-%m-%d %r")AS comment_time,user_name FROM comment_table 
+        JOIN user_table ON comment_table.user_index = user_table.user_index WHERE board_index=?`;
         var commentResult = await pool.queryParam_Arr(commentQuery,[board_index]);
         if(!commentResult){
             res.status(500).send("fail showing comment from server");
